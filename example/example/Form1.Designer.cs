412dbb87e9e18182bd8b0bacb478238a1da3bcae
sleepinguser <63859282+sleepinguser@users.noreply.github.com>
2021-01-20 19:38:13 +0600

    Add files via upload

---
+++ example/example/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(208, 156);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ example/example/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Permissions;
+
+namespace example
+{
+    public partial class Form1 : Form
+    {
+
+        FileSystemWatcher _fileWatcher;
+        public Form1()
+        {
+            InitializeComponent();
+            Watcher();
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public void Watcher()
+        {
+            _fileWatcher = new FileSystemWatcher(@"C:\libra");
+            _fileWatcher.NotifyFilter = NotifyFilters.LastWrite;
+            _fileWatcher.Filter = "temp.txt";
+            _fileWatcher.Changed += OnChanged;
+            _fileWatcher.EnableRaisingEvents = true;
+        }
+
+        public void OnChanged(object source, FileSystemEventArgs e)
+        {
+            try
+            {
+                string nomerpath = @"C:\libra\temp.txt";
+                string Numbers;
+                using (StreamReader sr = new StreamReader(nomerpath))
+                {
+                    Numbers = sr.ReadLine();
+                    MessageBox.Show(Numbers);
+                }
+                comboBox1.Text = Numbers;
+                _fileWatcher.EnableRaisingEvents = false;
+            }
+
+            catch (Exception ex)
+            {
+                _fileWatcher.EnableRaisingEvents = true;
+            }
+        }
+    }
+}
